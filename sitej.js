a00cbe5068902b6591e3c7ee4558b75b210af05b
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2015-12-31 23:20:25 -0500

    And, I improve it a little. last one

--- sitej.js
+++ sitej.js
@@ -14,6 +14,7 @@ function refresh() {
     } else if ($byid(section)){
         if ($byq("#words #"+section)) {
             window.location.href="#words";
+            $byid("hr").addclass("hidden");
             var href = $byid(section).attr("href");
             loadcontent(href, true);
             /*setTimeout(function(){
